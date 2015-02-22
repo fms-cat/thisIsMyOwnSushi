069c965da0111e0b196baa4a0157a0a0d018eb8a
FMS_Cat <ev40574@meiji.ac.jp>
2015-02-23 02:13:50 +0900

    yooooo there are bad uniform and define

--- thisIsMyOwnSushi.glsl
+++ thisIsMyOwnSushi.glsl
@@ -10,17 +10,14 @@ precision mediump float;
 #endif
 
 uniform float time;
-uniform vec2 mouse;
 uniform vec2 resolution;
 
-#define C vec3(.5,.1,.2)*2.
 #define ITER 72
 #define SITER 32
 
 #define dance (t*1.)
 
-#define tMax 0.
-#define t mod(time,tMax)
+#define t time
 #define r resolution.xy
 #define v vec2(0.,1.)
 #define cl(i) clamp(i,0.,1.)
@@ -62,8 +59,8 @@ float noiseT(vec2 _v)
 }
 
 float smin(float d1, float d2, float k){
- float h = exp(-k * d1) + exp(-k * d2);
- return -log(h) / k;
+  float h = exp(-k * d1) + exp(-k * d2);
+  return -log(h) / k;
 }
 
 vec2 rotate(vec2 i,float th)
@@ -94,15 +91,15 @@ vec2 geta(vec3 p)
 
 vec2 shari(vec3 p)
 {
-    float dis = sphere(p,.02);
-    float ii = 0.;
-    for( int i=0; i<26; i++ )
-    {
-        ii += 1.;
-  vec3 pt=vec3(sin(hash(ii*v.yy)*dance),sin(hash(ii*1.13*v.yy)*dance),sin(hash(ii*1.23*v.yy)*dance));
-        dis = smin( dis, sphere( p-pt*vec3(.17,.06,.09),.02 ), 49. );
-    }
-    return vec2(dis,1.);
+  float dis = sphere(p,.02);
+  float ii = 0.;
+  for( int i=0; i<26; i++ )
+  {
+    ii += 1.;
+    vec3 pt=vec3(sin(hash(ii*v.yy)*dance),sin(hash(ii*1.13*v.yy)*dance),sin(hash(ii*1.23*v.yy)*dance));
+    dis = smin( dis, sphere( p-pt*vec3(.17,.06,.09),.02 ), 49. );
+  }
+  return vec2(dis,1.);
 }
 
 vec2 neta(vec3 p)
@@ -128,135 +125,135 @@ vec3 getNormal(vec3 p,float d)
     distFunc(p+vec3(d,0.,0.)).x-distFunc(p+vec3(-d,0.,0.)).x,
     distFunc(p+vec3(0.,d,0.)).x-distFunc(p+vec3(0.,-d,0.)).x,
     distFunc(p+vec3(0.,0.,d)).x-distFunc(p+vec3(0.,0.,-d)).x
-  ));
-}
-
-float shadow(vec3 _ray,vec3 _lig,float k)
-{
-  vec3 ray=normalize(_lig-_ray);
-  float maxL=length(_lig-_ray);
-  float ret=1.;
-  float dist=0.;
-  float rayL=0.;
-  vec3 rayP=_ray;
-  for(int c=0;c<SITER;c++)
-  {
-    float dist=distFunc(rayP).x;
-    if(dist<1E-4){return 0.;}
-    rayL+=dist;
-    rayP=_ray+ray*rayL;
-    ret=min(ret,dist*k/rayL);
-    if(maxL<rayL){return ret;}
-  }
-  return 0.;
-}
-
-float plasma(vec3 p)
-{
-  return cl(sin(length((p-v.yyy*vec3(.4,.2,.7))*200.))*4.6+sin(p.x*30.+t)*3.9+sin((p.x*sin(t)+p.y*cos(t*.7)+p.z*.7)*64.)*6.4+.4);
-}
-
-vec3 shadeGeta(vec3 rayP,vec3 camP)
-{
-  vec3 col=v.xxx;
-
-  vec3 nor=getNormal(rayP,1E-4);
-
-  vec3 lig=v.xyx;
-  float dif=cl(dot(normalize(lig-rayP),nor))*.3;
-  float spe=pow(cl(dot(normalize(normalize(lig-rayP)+normalize(camP-rayP)),nor)),3E1);
-  float sha=shadow(rayP+1E-3*nor,lig,1E1);
-  float dec=exp(-length(lig-rayP)*.1);
-
-  vec3 base=vec3(.7,.3,.1)+plasma(rayP)*vec3(.0,.7,.2);
-  col+=((dif+1.)*(sha*.5+.5)*dec*base+spe*v.yyy)*v.yyx;
-
-  return col;
-}
-
-vec3 shadeShari(vec3 rayP,vec3 camP)
-{
-  vec3 col=v.xxx;
-
-  vec3 nor=getNormal(rayP,1E-4);
-
-  vec3 lig=v.xyx;
-  float dif=cl(dot(normalize(lig-rayP),nor))*.3;
-  float spe=pow(cl(dot(normalize(normalize(lig-rayP)+normalize(camP-rayP)),nor))*1.02,9E1);
-  float sha=shadow(rayP+1E-3*nor,lig,1E1);
-  float dec=exp(-length(lig-rayP)*.1);
-
-  col+=((dif*.2+.8)*(sha*.1+.3)*dec*v.yyy+spe*v.yyy);
-
-  return col;
-}
-
-vec3 shadeNeta(vec3 rayP,vec3 camP)
-{
-  vec3 col=v.xxx;
-
-  vec3 nor=getNormal(rayP,1E-4);
-
-  vec3 lig=v.xyx;
-  float dif=cl(dot(normalize(lig-rayP),nor))*.3;
-  float spe=pow(cl(dot(normalize(normalize(lig-rayP)+normalize(camP-rayP)),nor)),3E1);
-  float sha=shadow(rayP+1E-3*nor,lig,1E1);
-  float dec=exp(-length(lig-rayP)*.1);
-  col+=((dif+.5)*(sha*.5+.5)*dec*v.yxx+spe*v.yyy);
-
-  return col;
-}
-
-void main()
-{
-  vec2 p=(gl_FragCoord.xy*2.-r)/r.x;
-
-  float camR=1.+sin(dance*2.+1.5)*.2;
-  float camT=t*.8;
-
-  vec3 camP=vec3(cos(camT),.3,sin(camT))*camR;
-  vec3 camC=vec3(0.,-.1,0.);
-  vec3 camA=vec3(0.,1.,0.);
-  vec3 camS=cross(normalize(camC-camP),camA);
-  vec3 camU=cross(camS,normalize(camC-camP));
-  vec3 ray=normalize(camS*p.x+camU*p.y+normalize(camC-camP));
-
-  vec2 dist=v.xx;
-  float rayL=0.;
-  vec3 rayP=camP;
-  for(int i=0;i<ITER;i++)
-  {
-    dist=distFunc(rayP);
-    rayL+=dist.x*.8;
-    if(length(camP)*10.<rayL){ break; }
-    rayP=camP+ray*rayL;
+    ));
   }
 
-  vec3 col=v.xxx;
-
-  if(abs(dist.x)<1E-3)
+  float shadow(vec3 _ray,vec3 _lig,float k)
   {
-    float dis=1.6/clamp(rayL,1.8,99.);
-    if(dist.y==0.)
+    vec3 ray=normalize(_lig-_ray);
+    float maxL=length(_lig-_ray);
+    float ret=1.;
+    float dist=0.;
+    float rayL=0.;
+    vec3 rayP=_ray;
+    for(int c=0;c<SITER;c++)
     {
-      col+=shadeGeta(rayP,camP);
+      float dist=distFunc(rayP).x;
+      if(dist<1E-4){return 0.;}
+      rayL+=dist;
+      rayP=_ray+ray*rayL;
+      ret=min(ret,dist*k/rayL);
+      if(maxL<rayL){return ret;}
     }
-    if(dist.y==1.)
-    {
-      col+=shadeShari(rayP,camP);
-    }
-    if(dist.y==2.)
-    {
-      col+=shadeNeta(rayP,camP);
-    }
+    return 0.;
   }
-  else
+
+  float plasma(vec3 p)
   {
-    col=vec3(.1,.8,.4);
-    col+=cl((sin(atan(p.y,p.x)*40.+length(p)*20.+t*10.))*9.)*vec3(.1,-.3,.4);
+    return cl(sin(length((p-v.yyy*vec3(.4,.2,.7))*200.))*4.6+sin(p.x*30.+t)*3.9+sin((p.x*sin(t)+p.y*cos(t*.7)+p.z*.7)*64.)*6.4+.4);
   }
 
-  col-=length(p)*.2*v.yyy;
+  vec3 shadeGeta(vec3 rayP,vec3 camP)
+  {
+    vec3 col=v.xxx;
 
-  gl_FragColor=vec4(col,1.);
-}
+    vec3 nor=getNormal(rayP,1E-4);
+
+    vec3 lig=v.xyx;
+    float dif=cl(dot(normalize(lig-rayP),nor))*.3;
+    float spe=pow(cl(dot(normalize(normalize(lig-rayP)+normalize(camP-rayP)),nor)),3E1);
+    float sha=shadow(rayP+1E-3*nor,lig,1E1);
+    float dec=exp(-length(lig-rayP)*.1);
+
+    vec3 base=vec3(.7,.3,.1)+plasma(rayP)*vec3(.0,.7,.2);
+    col+=((dif+1.)*(sha*.5+.5)*dec*base+spe*v.yyy)*v.yyx;
+
+    return col;
+  }
+
+  vec3 shadeShari(vec3 rayP,vec3 camP)
+  {
+    vec3 col=v.xxx;
+
+    vec3 nor=getNormal(rayP,1E-4);
+
+    vec3 lig=v.xyx;
+    float dif=cl(dot(normalize(lig-rayP),nor))*.3;
+    float spe=pow(cl(dot(normalize(normalize(lig-rayP)+normalize(camP-rayP)),nor))*1.02,9E1);
+    float sha=shadow(rayP+1E-3*nor,lig,1E1);
+    float dec=exp(-length(lig-rayP)*.1);
+
+    col+=((dif*.2+.8)*(sha*.1+.3)*dec*v.yyy+spe*v.yyy);
+
+    return col;
+  }
+
+  vec3 shadeNeta(vec3 rayP,vec3 camP)
+  {
+    vec3 col=v.xxx;
+
+    vec3 nor=getNormal(rayP,1E-4);
+
+    vec3 lig=v.xyx;
+    float dif=cl(dot(normalize(lig-rayP),nor))*.3;
+    float spe=pow(cl(dot(normalize(normalize(lig-rayP)+normalize(camP-rayP)),nor)),3E1);
+    float sha=shadow(rayP+1E-3*nor,lig,1E1);
+    float dec=exp(-length(lig-rayP)*.1);
+    col+=((dif+.5)*(sha*.5+.5)*dec*v.yxx+spe*v.yyy);
+
+    return col;
+  }
+
+  void main()
+  {
+    vec2 p=(gl_FragCoord.xy*2.-r)/r.x;
+
+    float camR=1.+sin(dance*2.+1.5)*.2;
+    float camT=t*.8;
+
+    vec3 camP=vec3(cos(camT),.3,sin(camT))*camR;
+    vec3 camC=vec3(0.,-.1,0.);
+    vec3 camA=vec3(0.,1.,0.);
+    vec3 camS=cross(normalize(camC-camP),camA);
+    vec3 camU=cross(camS,normalize(camC-camP));
+    vec3 ray=normalize(camS*p.x+camU*p.y+normalize(camC-camP));
+
+    vec2 dist=v.xx;
+    float rayL=0.;
+    vec3 rayP=camP;
+    for(int i=0;i<ITER;i++)
+    {
+      dist=distFunc(rayP);
+      rayL+=dist.x*.8;
+      if(length(camP)*10.<rayL){ break; }
+      rayP=camP+ray*rayL;
+    }
+
+    vec3 col=v.xxx;
+
+    if(abs(dist.x)<1E-3)
+    {
+      float dis=1.6/clamp(rayL,1.8,99.);
+      if(dist.y==0.)
+      {
+        col+=shadeGeta(rayP,camP);
+      }
+      if(dist.y==1.)
+      {
+        col+=shadeShari(rayP,camP);
+      }
+      if(dist.y==2.)
+      {
+        col+=shadeNeta(rayP,camP);
+      }
+    }
+    else
+    {
+      col=vec3(.1,.8,.4);
+      col+=cl((sin(atan(p.y,p.x)*40.+length(p)*20.+t*10.))*9.)*vec3(.1,-.3,.4);
+    }
+
+    col-=length(p)*.2*v.yyy;
+
+    gl_FragColor=vec4(col,1.);
+  }
